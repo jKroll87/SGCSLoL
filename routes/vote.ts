bf120d9a809230f24334675ee495e5d8dcf09cdf
jKroll87 <bingquadog@gmail.com>
2021-02-01 23:20:57 +0900

    vote

--- routes/vote.ts
+++ routes/vote.ts
@@ -1,10 +1,22 @@
 import * as express from 'express';
+import * as mysql from 'mysql';
+import * as config from '../config/config.js';
+
 let router = express.Router();
+const connection = mysql.createConnection(config);
 
 router.get('/vote', (req, res, next) => {
-    res.render('vote'
-        
-    );
+    connection.query('', (error, rows) => {
+        if (error) {
+            console.log("Error", error);
+            res.send({
+                "code": 400,
+                "error": "Something wrong!"
+            })
+        } else {
+            //
+        };
+    });
 });
 
 export { router };
