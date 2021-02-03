d2af5d9cf4e9b8f63513fc4dffa5766b04c0bf9d
dongzoolee <nant0313@gmail.com>
2021-02-04 03:04:51 +0900

    Escape SQL Injection

--- routes/vote.ts
+++ routes/vote.ts
@@ -16,9 +16,9 @@ router.post('/vote', (req, res, next) => {
     let drake = req.body.drake;
     // let now = Date.now();
     // let date = new Date(now);
-    let query = 'INSERT INTO dragon (id, game, dragon, date) VALUES ("00000000", 0, ' + drake + ', ' + '"2000-01-01 00:00:00"' + ')';
-
-    connection.query(query, (error, rows) => {
+    let query = 'INSERT INTO dragon (id, game, dragon, date) VALUES (?, ?, ?, ?)';
+    // Escape SQL injection
+    connection.query(query, ["00000000", 0, drake, "2000-01-01 00:00:00"], (error, rows) => {
         if (error) {
             console.log("Error", error);
             res.send({
