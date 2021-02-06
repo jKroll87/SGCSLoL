bfded9fdf33b8ceb613746eec083f32493be958a
jKroll87 <bingquadog@gmail.com>
2021-02-06 21:29:15 +0900

    add localeDate

--- routes/vote.ts
+++ routes/vote.ts
@@ -14,9 +14,12 @@ router.get('/vote', (req, res, next) => {
 
 router.post('/vote', (req, res, next) => {
     let drake = req.body.drake;
-    // let now = Date.now();
-    // let date = new Date(now);
-    let query = 'INSERT INTO dragon (id, game, dragon, date) VALUES ("00000000", 0, ' + drake + ', ' + '"2000-01-01 00:00:00"' + ')';
+    let now = Date.now();
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const kr_curr = new Date(now + (KR_TIME_DIFF));
+    let date = new Date(kr_curr).toISOString().slice(0, 19).replace('T', ' ');
+    console.log(date);
+    let query = 'INSERT INTO dragon (id, game, dragon, date) VALUES ("00000000", 0, ' + drake + ', "' + date + '")';
 
     connection.query(query, (error, rows) => {
         if (error) {
